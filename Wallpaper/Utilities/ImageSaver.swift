83eeb842313fd41964d6601f45c0a6c8f2c0a6ed
David Spry <dspry@gmx.com>
2022-05-01 12:31:10 +1000

    Amend image saving in `ImageSaver`.

--- Wallpaper/Utilities/ImageSaver.swift
+++ Wallpaper/Utilities/ImageSaver.swift
@@ -9,13 +9,13 @@ import Cocoa
 
 struct ImageSaver {
     static private let imageProperties: [NSBitmapImageRep.PropertyKey: Any] = [
-        .compressionFactor: NSNumber(floatLiteral: 0.8)
+        .compressionFactor: NSNumber(floatLiteral: 1.0)
     ]
 
     static public func saveImageAsFile(_ image: NSImage) {
         guard let imageAsTiff = image.tiffRepresentation,
               let imageAsBitmap = NSBitmapImageRep(data: imageAsTiff),
-              let imageAsData = imageAsBitmap.representation(using: .png, properties: imageProperties),
+              let imageAsData = imageAsBitmap.representation(using: .jpeg, properties: imageProperties),
               let window = NSApplication.shared.mainWindow else {
             return
         }
@@ -25,23 +25,27 @@ struct ImageSaver {
                 guard let panelUrl = panel.url else {
                     let alert = NSAlert()
                     alert.alertStyle = .critical
-                    alert.messageText = "NSSavePanel did not return a URL. Sorry!"
+                    alert.messageText = "NSSavePanel did not return a URL."
                     alert.runModal()
                     return
                 }
-
+                
                 do { try imageAsData.write(to: panelUrl) } catch {
                     print("Error: Could not write tiled image to file. \(error.localizedDescription)")
                 }
             }
         }
     }
+    
+    static private func didSetFileExtension(to fileExtension: String, forPanel panel: NSSavePanel) {
+        panel.allowedFileTypes = [fileExtension]
+    }
 
     static private func SavePanel(overWindow window: NSWindow, then handleResult: @escaping (_ panel: NSSavePanel, _ response: NSApplication.ModalResponse) -> Void) {
         let panel = NSSavePanel()
-        panel.title = "Save Tiled Image"
+        panel.title = "Save Image"
         panel.nameFieldLabel = "Filename:"
-        panel.nameFieldStringValue = "TiledImage"
+        panel.nameFieldStringValue = "Wallpaper"
         panel.prompt = "Save"
         panel.allowedFileTypes = ["jpg"]
         panel.isExtensionHidden = false
